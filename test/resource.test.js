08e10d7dd99855be5d51e68cd869940bf12e83fb
Laxmiraj <ulaxmira@LAMU02DW8X7MD6R.uhc.com>
2021-10-19 22:52:13 +0530

    Code synced with UI code

--- test/resource.test.js
+++ test/resource.test.js
@@ -3,148 +3,128 @@ const action = require("../helpers/actionHelpers");
 const ResourcePage = require("../pages/benefit.page");
 const clientData = require("../client.data");
 const fs = require("fs");
-const path = require('path');
+const path = require("path");
 const rallyUtil = require("../helpers/rallyHelpers");
 const rof = require("../main/rof");
 const constants = require("../constants");
-const launchDate = require("../testdata/generic.json")
-
+const launchDate = require("../testdata/generic.json");
 
 describe("Implementation", () => {
   try {
     const out = "./../testdata/expected/resource.json";
     const outPath = path.resolve(__dirname, out);
-      describe("clientName", () => {
-        it("Support Details Page", () => {
-          rof.loginSalesforce(
-            constants.username,
-            constants.password,
-          );
-          rallyUtil.saveClientDetailsFromSF("resource");
+    describe("clientName", () => {
+      it("Support Details Page", () => {
+        rof.loginSalesforce(constants.username, constants.password);
+        rallyUtil.saveClientDetailsFromSF("resource");
+        let Json1 = fs.readFileSync(outPath)
+        let objectJson = JSON.parse(Json1)
+        console.log("Some value: " + objectJson);
 
-          //Validation
-
+        //Validation
+        for (let key in objectJson) {
+        if (action.isArray(objectJson[key])) {
+          for (arrCount = 0; arrCount < objectJson[key].length; arrCount++) {
+            userName = objectJson[key][arrCount].username;
+            password = objectJson[key][arrCount].password;
+            contactNumber = objectJson[key][arrCount].contactNumber;
+            resourceHeadline = objectJson[key][arrCount].resourceHeadline;
+            resourceBody = objectJson[key][arrCount].resourceBody;
+            try {
+              if (resourceHeadline == null) {
+                //Rally UI Validation
+                src.Login(clientData.LoginURL, userName, password);
+                src.ResourcePage();
+                browser.takeScreenshot();
+                console.log(
+                  clientName +
+                    " Benefits Page Validation Completed Successfully"
+                );
+                browser.reloadSession();
+              } else {
+                console.log("Client has Custom resource Page");
+                src.Login(clientData.LoginURL, userName, password);
+                src.ResourcePage();
+                action.doWaitForElement($(ResourcePage.headline));
+                const ResourcePageHeadline = action.doGetText(
+                  $(ResourcePage.headline)
+                );
+                const ResourcePageBodyText = action.doGetText(
+                  $(ResourcePage.bodytext)
+                );
+                browser.takeScreenshot();
+                assert.equal(
+                  resourceHeadline,
+                  ResourcePageHeadline,
+                  "Invalid page Headline"
+                );
+                assert.equal(
+                  resourceBody,
+                  ResourcePageBodyText,
+                  "Invalid page Body Text"
+                );
+                console.log(
+                  clientName +
+                    " Benefits Page Validation Completed Successfully"
+                );
+                browser.reloadSession();
+              }
+            } catch (exception) {
+              browser.reloadSession();
+              throw exception;
+            }
+          }
+        } else {
+          var userName = objectJson[key].username;
+          var password = objectJson[key].password;
+          var resourceHeadline = objectJson[key].resourceHeadline;
+          var resourceBody = objectJson[key].resourceBody;
           try {
-            let objectJson = fs.readFileSync(outPath);
-            console.log("Some value: " + objectJson);
-            // for (let key in objectJson) {
-            //   const clientName = JSON.stringify(key) + " - " + launchDate.launchDate;
-            //   describe(clientName, () => {
-            //     it("Benefits Page", () => {
-            //       console.log("Implementation : " + clientName);
-            //       if (action.isArray(objectJson[key])) {
-            //         for (arrCount = 0; arrCount < objectJson[key].length; arrCount++) {
-            //           userName = objectJson[key][arrCount].username;
-            //           password = objectJson[key][arrCount].password;
-            //           contactNumber = objectJson[key][arrCount].contactNumber;
-            //           resourceHeadline = objectJson[key][arrCount].resourceHeadline;
-                     
-            //           resourceBody = objectJson[key][arrCount].resourceBody;
-            //           try {
-            //             if (resourceHeadline === "null") {
-            //               //Rally UI Validation
-            //               src.Login(clientData.LoginURL, userName, password);
-            //               src.ResourcePage();
-            //               browser.takeScreenshot();
-            //               console.log(
-            //                 clientName +
-            //                   " Benefits Page Validation Completed Successfully"
-            //               );
-            //               browser.reloadSession();
-            //             } else {
-            //               console.log("Client has Custom resource Page");
-            //               src.Login(clientData.LoginURL, userName, password);
-            //               src.ResourcePage();
-            //               action.doWaitForElement($(ResourcePage.headline));
-            //               const ResourcePageHeadline = action.doGetText(
-            //                 $(ResourcePage.headline)
-            //               );
-            //               const ResourcePageBodyText = action.doGetText(
-            //                 $(ResourcePage.bodytext)
-            //               );
-            //               browser.takeScreenshot();
-            //               assert.equal(
-            //                 resourceHeadline,
-            //                 ResourcePageHeadline,
-            //                 "Invalid page Headline"
-            //               );
-            //               assert.equal(
-            //                 resourceBody,
-            //                 ResourcePageBodyText,
-            //                 "Invalid page Body Text"
-            //               );
-            //               console.log(
-            //                 clientName +
-            //                   " Benefits Page Validation Completed Successfully"
-            //               );
-            //               browser.reloadSession();
-            //             }
-            //           } catch (exception) {
-            //             browser.reloadSession();
-            //             throw exception;
-            //           }
-            //         }
-            //       } else {
-            //         userName = objectJson[key].username;
-            //         password = objectJson[key].password;
-            //         contactNumber = objectJson[key].contactNumber;
-                    
-            //         resourceHeadline01 = objectJson[key].resourceHeadline;
-            //         console.log(resourceHeadline01)
-            //         resourceBody = objectJson[key].resourceBody;
-            //       //   try {
-            //       //     if (resourceHeadline === "null") {
-            //       //       //Rally UI Validation
-            //       //       src.Login(clientData.LoginURL, userName, password);
-            //       //       src.ResourcePage();
-            //       //       browser.takeScreenshot();
-            //       //       console.log(
-            //       //         clientName +
-            //       //           " Benefits Page Validation Completed Successfully"
-            //       //       );
-            //       //       browser.reloadSession();
-            //       //     } else {
-            //       //       console.log("custom resource Page");
-            //       //       src.Login(clientData.LoginURL, userName, password);
-            //       //       src.ResourcePage();
-            //       //       action.doWaitForElement($(ResourcePage.headline));
-            //       //       const ResourcePageHeadline = action.doGetText(
-            //       //         $(ResourcePage.headline)
-            //       //       );
-            //       //       const ResourcePageBodyText = action.doGetText(
-            //       //         $(ResourcePage.bodytext)
-            //       //       );
-            //       //       browser.takeScreenshot();
-            //       //       assert.equal(
-            //       //         resourceHeadline,
-            //       //         ResourcePageHeadline,
-            //       //         "Invalid page Headline"
-            //       //       );
-            //       //       assert.equal(
-            //       //         resourceBody,
-            //       //         ResourcePageBodyText,
-            //       //         "Invalid page Body Text"
-            //       //       );
-            //       //       console.log(
-            //       //         clientName +
-            //       //           " Benefits Page Validation Completed Successfully"
-            //       //       );
-            //       //       browser.reloadSession();
-            //       //     }
-            //       //   } catch (exception) {
-            //       //     browser.reloadSession();
-            //       //     throw exception;
-            //       //   }
-            //        }
-            //     });
-            //   });
-            // }
+            if (resourceHeadline == null) {
+              //Rally UI Validation
+              console.log(userName)
+              src.Login(clientData.LoginURL, userName, password);
+              src.ResourcePage();
+              browser.takeScreenshot();
+              console.log(
+                " Benefits Page Validation Completed Successfully"
+              );
+              browser.reloadSession();
+            } else {
+              console.log("custom resource Page");
+              src.Login(clientData.LoginURL, userName, password);
+              src.ResourcePage();
+              action.doWaitForElement($(ResourcePage.headline));
+              const ResourcePageHeadline = action.doGetText(
+                $(ResourcePage.headline)
+              );
+              const ResourcePageBodyText = action.doGetText(
+                $(ResourcePage.bodytext)
+              );
+              browser.takeScreenshot();
+              assert.equal(
+                resourceHeadline,
+                ResourcePageHeadline,
+                "Invalid page Headline"
+              );
+              assert.equal(
+                resourceBody,
+                ResourcePageBodyText,
+                "Invalid page Body Text"
+              );
+              console.log(
+                " Benefits Page Validation Completed Successfully"
+              );
+              browser.reloadSession();
+            }
           } catch (exception) {
             browser.reloadSession();
             throw exception;
           }
-        });
+        }
+      }
       });
+    });
   } catch (exception) {
     browser.reloadSession();
     throw exception;
--- test/support.test.js
+++ test/support.test.js
@@ -1,27 +1,74 @@
-const clientData = require("../client.data");
 const src = require("../main/sourse");
 const action = require("../helpers/actionHelpers");
 const supportPage = require("../pages/support.page");
+const clientData = require("../client.data");
 const fs = require("fs");
+const path = require("path");
 const rallyUtil = require("../helpers/rallyHelpers");
 const rof = require("../main/rof");
 const constants = require("../constants");
-
+const launchDate = require("../testdata/generic.json");
 
 describe("Implementation", () => {
   try {
-      describe("clientName", () => {
-        it("Support Details Page", () => {
-          rof.loginSalesforce(
-            constants.username,
-            constants.password,
-          );
-          rallyUtil.saveClientDetailsFromSF("support");
+    const out = "./../testdata/expected/support.json";
+    const outPath = path.resolve(__dirname, out);
+    describe("clientName", () => {
+      it("Support Details Page", () => {
+        rof.loginSalesforce(constants.username, constants.password);
+        rallyUtil.saveClientDetailsFromSF("support");
+        let Json1 = fs.readFileSync(outPath);
+        let objectJson = JSON.parse(Json1);
+        console.log("Some value: " + objectJson);
 
-          // Validations
-
-        });
+        // Validations
+        for (let key in objectJson) {
+          if (action.isArray(objectJson[key])) {
+            for (arrCount = 0; arrCount < objectJson[key].length; arrCount++) {
+              userName = objectJson[key][arrCount].username;
+              password = objectJson[key][arrCount].password;
+              contactNumber = objectJson[key][arrCount].contactNumber
+                .replace(/[^0-9]/g, "")
+                .substring(1);
+              // Rally UI Validation
+              src.Login(clientData.LoginURL, userName, password);
+              src.SupportPage();
+              const RSupportNumber = action
+                .doGetText($(supportPage.contactSupportNumber))
+                .replace(/[^a-zA-Z0-9]/g, "");
+              browser.takeScreenshot();
+              assert.equal(
+                contactNumber,
+                RSupportNumber,
+                "Invalid Customer Support Number"
+              );
+              console.log(" Support Details Validation Completed Successfully");
+              browser.reloadSession();
+            }
+          } else {
+            userName = objectJson[key].username;
+            password = objectJson[key].password;
+            contactNumber = objectJson[key].contactNumber
+              .replace(/[^0-9]/g, "")
+              .substring(1);
+            console.log(contactNumber);
+            src.Login(clientData.LoginURL, userName, password);
+            src.SupportPage();
+            const RSupportNumber = action
+              .doGetText($(supportPage.contactSupportNumber))
+              .replace(/[^a-zA-Z0-9]/g, "");
+            browser.takeScreenshot();
+            assert.equal(
+              contactNumber,
+              RSupportNumber,
+              "Invalid Customer Support Number"
+            );
+            console.log(" Support Details Validation Completed Successfully");
+            browser.reloadSession();
+          }
+        }
       });
+    });
   } catch (exception) {
     browser.reloadSession();
     throw exception;
